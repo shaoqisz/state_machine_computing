c2c0422d32ad5ff1e89f62e72e0de8602dab826c
Qi.Shao <qi.shao@philips.com>
2025-04-17 23:58:21 +0800

    add example of using the custom matter

---
+++ config/matter/custom_matter.py
@@ -0,0 +1,54 @@
+import sys
+
+
+def T1():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+
+def T2():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return False
+
+def T3():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T4():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T5():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T6():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+
+def T7():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T8():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T9():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T10():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T11():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T12():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T13():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T14():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T15():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
+def T16():
+    print(f'calling {sys._getframe().f_code.co_name}')
+    return True
