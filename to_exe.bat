8978e1f4749855900e32f9bfde83e752c8683712
Qi.Shao <qi.shao@philips.com>
2025-04-19 19:28:54 +0800

    copy the stuffs to the dist folder

--- to_exe.bat
+++ to_exe.bat
@@ -1,3 +1,6 @@
 pyinstaller -F -w state_machine_ui.py  -i="sm.ico" -n state_machine_computing --distpath dist
 xcopy /i /y sm.ico                      dist\
-
+xcopy /i /y sm.png                      dist\
+xcopy /i /y config.json                 dist\
+xcopy /s /e /i  config                  dist\
+xcopy /s /e /i  res                     dist\
