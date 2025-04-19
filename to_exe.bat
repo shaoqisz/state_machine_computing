7775c35216c966c01ed87c0af2c52003352bb48f
Qi.Shao <qi.shao@philips.com>
2025-04-19 20:04:03 +0800

    correct the path of dest

--- to_exe.bat
+++ to_exe.bat
@@ -2,5 +2,5 @@ pyinstaller -F -w state_machine_ui.py  -i="sm.ico" -n state_machine_computing --
 xcopy /i /y sm.ico                      dist\
 xcopy /i /y sm.png                      dist\
 xcopy /i /y config.json                 dist\
-xcopy /s /e /i  config                  dist\
-xcopy /s /e /i  res                     dist\
+xcopy /s /e /i  config                  dist\config
+xcopy /s /e /i  res                     dist\res
