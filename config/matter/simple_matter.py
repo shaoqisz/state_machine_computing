cef23351b6dec109c495974eeba3577215e03c10
Qi.Shao <qi.shao@philips.com>
2025-04-20 08:00:30 +0800

    remove config

--- config/matter/simple_matter.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-def state1_state2_trans(actions):
-    print(f'calling custom\'s transition - {sys._getframe().f_code.co_name}()')
-    actions.append("Custom action is executed during the transition.")
-    return True
--- to_exe.bat
+++ to_exe.bat
@@ -1,6 +1,4 @@
 pyinstaller -F -w state_machine_ui.py  -i="sm.ico" -n state_machine_computing --distpath dist
 xcopy /i /y sm.ico                      dist\
 xcopy /i /y sm.png                      dist\
-xcopy /i /y config.json                 dist\
-xcopy /s /e /i  config                  dist\config
 xcopy /s /e /i  res                     dist\res
